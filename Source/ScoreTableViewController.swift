61f44166c6ae32886e42e1f108deef259a77d8cc
Jakub Dolejs <jakubdolejs@gmail.com>
2019-03-29 11:22:04 +0000

    Added FAR table

---
+++ Source/ScoreTableViewController.swift
@@ -0,0 +1,59 @@
+//
+//  ScoreTableViewController.swift
+//  Ver-ID Credentials Sample
+//
+//  Created by Jakub Dolejs on 29/03/2019.
+//  Copyright © 2019 Applied Recognition Inc. All rights reserved.
+//
+
+import UIKit
+
+class ScoreTableViewController: UITableViewController {
+    
+    var score: CGFloat?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let score = self.score {
+            self.navigationItem.title = String(format: "Your score: %.01f", score)
+        }
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 13
+    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
