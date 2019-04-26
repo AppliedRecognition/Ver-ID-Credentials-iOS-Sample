e813638957700391fcbfefe40a8e773d7ce9a173
Jakub Dolejs <jakubdolejs@gmail.com>
2019-04-26 21:50:09 +0100

    Simplified drawing of the similarity dial

--- Source/SimiarityDial.swift
+++ Source/SimiarityDial.swift
@@ -32,46 +32,28 @@ class SimiarityDial: CAShapeLayer {
         if self.bounds.isEmpty {
             return
         }
-        let backgroundLayer = CAShapeLayer()
-        backgroundLayer.path = UIBezierPath(rect: bounds).cgPath
-        backgroundLayer.fillColor = UIColor(red: 0.21176470588235, green: 0.68627450980392, blue: 0.0, alpha: 1.0).cgColor
+        let lineWidth: CGFloat = 20
+        let arcCenter = CGPoint(x: self.bounds.midX, y: self.bounds.maxY)
+        let radius = min(self.bounds.midX, self.bounds.height) - lineWidth / 2
+        let startAngle = CGFloat.pi
+        let endAngle = CGFloat.pi * 2
+        let arcPath = UIBezierPath(arcCenter: arcCenter, radius: radius, startAngle: startAngle, endAngle: endAngle, clockwise: true).cgPath
         
-        let minAngle = CGFloat.pi
-        let maxAngle = CGFloat.pi * 2
-        let thresholdAngle = threshold / max * (maxAngle - minAngle)
-        let thresholdPoint = CGPoint(x: bounds.midX - cos(thresholdAngle)*bounds.width/2, y: bounds.minY + sin(thresholdAngle)*bounds.height*2)
-        let arcCentre = CGPoint(x: bounds.midX, y: bounds.minY)
-        let ovalRect = CGRect(x: bounds.minX, y: bounds.minY, width: bounds.width, height: bounds.height * 2)
+        let failLayer = CAShapeLayer()
+        failLayer.fillColor = nil
+        failLayer.strokeColor = UIColor.red.cgColor
+        failLayer.lineWidth = lineWidth
+        failLayer.strokeEnd = threshold / max
+        failLayer.path = arcPath
+        self.addSublayer(failLayer)
         
-        let maskPath = UIBezierPath(ovalIn: ovalRect)
-        maskPath.append(UIBezierPath(ovalIn: ovalRect.insetBy(dx: 20, dy: 20)).reversing())
-        maskPath.append(UIBezierPath(rect: CGRect(x: bounds.minX, y: bounds.maxY, width: bounds.width, height: bounds.height)).reversing())
-        let maskLayer = CAShapeLayer()
-        maskLayer.path = maskPath.cgPath
-        
-        let backgroundMaskLayer = CAShapeLayer()
-        backgroundMaskLayer.path = maskPath.cgPath
-        
-        let noPath = UIBezierPath()
-        noPath.move(to: arcCentre)
-        noPath.addLine(to: CGPoint(x: bounds.minX, y: bounds.minY))
-        noPath.addLine(to: CGPoint(x: bounds.minX, y: bounds.maxY))
-        if threshold / max > 0.5 {
-            noPath.addLine(to: CGPoint(x: bounds.maxX, y: bounds.maxY))
-        }
-        noPath.addLine(to: thresholdPoint)
-        noPath.close()
-        
-        let shapeLayer = CAShapeLayer()
-        shapeLayer.fillColor = UIColor.red.cgColor
-        let transform = CGAffineTransform(scaleX: 1, y: -1).concatenating(CGAffineTransform(translationX: 0, y: bounds.height))
-        noPath.apply(transform)
-        shapeLayer.path = noPath.cgPath
-        
-        self.addSublayer(backgroundLayer)
-        self.addSublayer(shapeLayer)
-        backgroundLayer.mask = backgroundMaskLayer
-        shapeLayer.mask = maskLayer
+        let passLayer = CAShapeLayer()
+        passLayer.fillColor = nil
+        passLayer.strokeColor = UIColor(red: 0.21176470588235, green: 0.68627450980392, blue: 0.0, alpha: 1.0).cgColor
+        passLayer.lineWidth = lineWidth
+        passLayer.strokeStart = threshold / max
+        passLayer.path = arcPath
+        self.addSublayer(passLayer)
         
         let dotRadius: CGFloat = 12
         let dotPath = UIBezierPath(ovalIn: CGRect(x: bounds.midX-dotRadius/2, y: bounds.maxY-dotRadius/2, width: dotRadius, height: dotRadius))
@@ -80,7 +62,7 @@ class SimiarityDial: CAShapeLayer {
         dotLayer.path = dotPath.cgPath
         self.addSublayer(dotLayer)
         
-        let scoreAngle = score / max * (maxAngle - minAngle)
+        let scoreAngle = score / max * (endAngle - startAngle)
         let scorePoint = CGPoint(x: bounds.midX - cos(scoreAngle)*bounds.width/2, y: bounds.maxY - sin(scoreAngle)*bounds.height)
         let needlePath = UIBezierPath()
         needlePath.move(to: CGPoint(x: bounds.midX, y: bounds.maxY))
